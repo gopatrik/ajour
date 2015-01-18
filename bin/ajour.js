063e5808409a52cb1c8fc66c413aea383fee67da
Patrik Gothe <patrik.got@gmail.com>
2015-01-18 21:21:06 +0100

    print error and manual if no message provided

--- bin/ajour.js
+++ bin/ajour.js
@@ -14,19 +14,27 @@ var Ajour = function () {
 	var timestamp = argv.t ? "" : '\n# ' + new Date() +'\n';
 
 	// join the _ argument string
-	var entry = argv._.join(' ') + '\n';
+	var message = argv._.join(' ') + '\n';
 
 	// join atoms into final entry string
-	var entry = timestamp + entry;
+	var entry = timestamp + message;
+
+	var manual = function () {
+		return "\tType\n\t$ ajour <your entry>\n \tto create an entry.";
+	};
 
 	var write = function () {
-		fs.appendFile('ajour.md', entry, function(err) {
-		    if(err) {
-		        console.log(err);
-		    } else {
-		        console.log('The file was saved!');
-		    }
-		}); 
+		if(argv._.length > 0){
+			fs.appendFile('ajour.md', entry, function(err) {
+			    if(err) {
+			        console.log(err);
+			    } else {
+			        console.log('ajour.md << ' + entry);
+			    }
+			}); 
+		}else{
+			console.log("Error: No message specified\n",manual());
+		}
 	};
 
 	return Object.freeze({
